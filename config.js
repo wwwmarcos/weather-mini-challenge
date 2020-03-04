3f3230ef6db9564302f1ab2bd200a0a72f9914e7
marcos <eptaccio@gmail.com>
2020-03-04 03:35:52 -0300

    add HUMIDITY_LIMIT env

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ const config = {
   APP_ID: process.env.APP_ID,
   API_URL: 'http://api.openweathermap.org/data/2.5/forecast',
   CITY: 'Ribeirão Preto',
-  DAYS_FUTURE_VERIFICATIONS: process.env.DAYS_FUTURE_VERIFICATIONS || 5
+  DAYS_FUTURE_VERIFICATIONS: process.env.DAYS_FUTURE_VERIFICATIONS || 5,
+  HUMIDITY_LIMIT: process.env.HUMIDITY_LIMIT || 70
 }
 
 module.exports = {
