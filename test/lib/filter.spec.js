df58c02a73f76204d7d0dda36f30512e6eb2decd
marcos <eptaccio@gmail.com>
2020-03-04 00:46:06 -0300

    start filter tests

--- test/lib/filter.spec.js
+++ test/lib/filter.spec.js
@@ -1,7 +1,41 @@
 const { filter } = require('../../lib')
+const { data } = require('./sampleData')
+
+const FIVE_DAYS = 5
+const SIX_DAYS = 6
+const SEVENTY_PER_CENT = 70
 
 describe('should filter data', () => {
-  it('should filter the next days', () => {
+  it('should filter the next n first days', () => {
+    const { list } = data
 
+    const nextFiveDays = filter.getNextDays({
+      list,
+      days: FIVE_DAYS
+    })
+
+    const nextSixDays = filter.getNextDays({
+      list,
+      days: SIX_DAYS
+    })
+
+    expect(nextFiveDays.length).toEqual(FIVE_DAYS)
+    expect(nextSixDays.length).toEqual(SIX_DAYS)
+  })
+
+  it('should filter by humidity', () => {
+    const days = [
+      { main: { humidity: 94 } },
+      { main: { humidity: 70 } },
+      { main: { humidity: 71 } },
+      { main: { humidity: 21 } }
+    ]
+
+    const result = filter.filterByHumidity({
+      greaterThan: SEVENTY_PER_CENT,
+      list: days
+    })
+
+    expect(result.length).toEqual(2)
   })
 })
